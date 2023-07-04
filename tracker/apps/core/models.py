9b168acfce111551eb4281b0d19c7ebff427ce0e
lmponcio <lmponcio@gmail.com>
2023-07-04 18:11:18 +1000

    feat(core): Configure Models. Refs #2
    Core app:
    - Create Models of Ticket, TicketStatus, and TicketComment
    - Updating .gitignore for migrations to be in VCS
    - Adding migrations of apps core and accounts.
    
    Refs #2

--- tracker/apps/core/models.py
+++ tracker/apps/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from tracker.apps.accounts.models import CustomUser
 
-# Create your models here.
+
+class TicketStatus(models.Model):
+    status = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=200)
+
+
+class Ticket(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.PROTECT, related_name="created_tickets")
+    status = models.ForeignKey(TicketStatus, on_delete=models.PROTECT)
+    assignees = models.ManyToManyField(CustomUser, blank=True, related_name="assigned_tickets")
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField()
+    content = models.TextField(max_length=2000)
+
+
+class TicketComment(models.Model):
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
+    author = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
+    pub_date = models.DateTimeField()
+    content = models.TextField(max_length=2000)
