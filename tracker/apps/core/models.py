682f20acda1e86651f7213f178aa11d4451d5efa
lmponcio <lmponcio@gmail.com>
2023-07-06 14:13:22 +1000

    feat(core) Remove redundant model fields. Refs #2

--- tracker/apps/core/models.py
+++ tracker/apps/core/models.py
@@ -26,7 +26,6 @@ class Ticket(TimeStampedModel):
     status = models.ForeignKey(TicketStatus, on_delete=models.PROTECT)
     assignees = models.ManyToManyField(CustomUser, blank=True, related_name="assigned_tickets")
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField()
     content = models.TextField(max_length=2000)
     # TODO:
     # add close_date (blank=True, null=True)
@@ -36,5 +35,4 @@ class Ticket(TimeStampedModel):
 class TicketComment(TimeStampedModel):
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
     author = models.ForeignKey(CustomUser, on_delete=models.PROTECT)
-    pub_date = models.DateTimeField()
     content = models.TextField(max_length=2000)
