f8528765dd103305168f40741dcf93944d48d36c
lmponcio <lmponcio@gmail.com>
2023-07-06 18:14:32 +1000

    feat(core): add Ticket Detail & List views. Refs #2
    Core app:
    - Create Views, Templates and UrlConfs of TicketListView and TicketDetail View
    - Reverse related object lookup in ticket_detail template (add it also in profile template)
    
    Refs #2

--- tracker/apps/core/urls.py
+++ tracker/apps/core/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import dashboard
+from .views import dashboard, TicketListView, TicketDetailView
 
 app_name = "core"
 
 urlpatterns = [
     path("", dashboard, name="dashboard"),
+    path("tickets/list/", TicketListView.as_view(), name="ticket-list"),
+    path("tickets/<pk>/", TicketDetailView.as_view(), name="ticket-detail"),
 ]
--- tracker/apps/core/views.py
+++ tracker/apps/core/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from .models import Ticket
 
 
 # to be removed (testing)
 def dashboard(request):
     return HttpResponse("Welcome to the Dashboard")
+
+
+class TicketListView(generic.ListView):
+    model = Ticket
+
+
+class TicketDetailView(generic.DetailView):
+    model = Ticket
+    context_object_name = "ticket"
