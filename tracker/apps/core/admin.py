b2942d74fbd68e511620ac4ea11746667aa89ac7
lmponcio <lmponcio@gmail.com>
2023-07-06 14:06:20 +1000

    feat(core) Register ticket models in admin. Refs #2

--- tracker/apps/core/admin.py
+++ tracker/apps/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TicketStatus, Ticket, TicketComment
+
+admin.site.register(TicketStatus)
+admin.site.register(Ticket)
+admin.site.register(TicketComment)
