aa3dfcfe4fdd00f782fb90b512f06a114c97782c
lmponcio <lmponcio@gmail.com>
2023-07-10 15:26:01 +1000

    feat(core): Update Dashboard View. Refs #3
    - Add CustomUser class method
    - Update Dashboard view
    - Update Dashboard Template
    
    Refs #3

--- tracker/apps/accounts/models.py
+++ tracker/apps/accounts/models.py
@@ -6,5 +6,15 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     pass
 
+    @classmethod
+    def get_users_with_assignments(cls):
+        uwa = {}
+        all = cls.objects.all().select_related()
+        for user in all:
+            tickets = user.assigned_tickets.filter(status__name="Open").order_by("-pub_date")
+            if tickets:
+                uwa[user.username] = tickets
+        return uwa
+
     def __str__(self):
         return self.username
